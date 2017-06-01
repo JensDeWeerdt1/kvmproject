d32f9e2f490cbea049d323c3c3c2c892db9b5ae7
Fabio Van Dooren <fabio.vandooren@hotmail.com>
2017-06-01 18:07:20 +0200

    branding logo returnt naar home ipv login

--- resources/views/play.blade.php
+++ resources/views/play.blade.php
@@ -1,12 +1,10 @@
 @extends('layouts.app')
-<?php
-header('X-Frame-Options: GOFORIT');
-?>
+
 @section('content')
     <div class="container videoplayer">
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
-                <iframe width="560" height="315" src="{!! $videoSrc !!}" frameborder="0" allowfullscreen></iframe>
+                <iframe width="560" height="315" src="{{ $videoSrc }}" frameborder="0" allowfullscreen></iframe>
                 <h2 class="videotitles">Football 360</h2>
             </div>
         </div>
