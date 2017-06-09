3eda53906494ef69120e120e5336e25a845a8277
Fabio Van Dooren <fabio.vandooren@hotmail.com>
2017-06-09 14:26:44 +0200

    seeds toegevoegd

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,12 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        $video1 = ['thuisPloeg' => 'KVM','uitPloeg' => 'Gent', 'stand' => '3-2', 'videoSrc' => 'https://www.youtube.com/embed/C3T467D8oqI?ecver=2'];
+        $video2 = ['thuisPloeg' => 'Standard','uitPloeg' => 'KVM', 'stand' => '1-2', 'videoSrc' => 'https://www.youtube.com/embed/E0HUVPM_A00?ecver=2'];
+        $video3 = ['thuisPloeg' => 'KVM','uitPloeg' => 'Lierse', 'stand' => '1-0', 'videoSrc' => 'https://www.youtube.com/embed/ebICMhOtipg?ecver=2'];
+        $db1 = DB::table('videos')->insert($video1);
+        $db2 = DB::table('videos')->insert($video2);
+        $db3 = DB::table('videos')->insert($video3);
+
     }
 }
