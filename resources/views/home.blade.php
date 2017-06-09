34496e8a4369ffc495f9b89bc006e1c75d29bd0b
Fabio Van Dooren <fabio.vandooren@hotmail.com>
2017-06-09 15:28:56 +0200

    home.blade.php aangepast met variabele data-elementen voor de matchen

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,29 +1,41 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+
+    <div class="container">
+        <div class="row">
+            <ul class="col-md-8 col-md-offset-2">
                 <div class="dropdown dropdown1">
-                  <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-                    Gespeelde matchen
-                    <span class="caret"></span>
-                  </button>
-                  <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
-                    <li><a href="#">Gespeelde matchen</a></li>
-                    <li><a href="#">Live</a></li>
-                  </ul>
+                    <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                        Gespeelde matchen
+                        <span class="caret"></span>
+                    </button>
+                    <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
+                        <li><a href="#">Gespeelde matchen</a></li>
+                        <li><a href="#">Live</a></li>
+                    </ul>
                 </div>
                 <div class="kalender">
                     <img  src="../../images/calendar.svg" alt="Kalender icoon">
                 </div>
-           <hr>
-             <ul class="flexcontainer">
-                <a href="#"><li class="flex-item"><img class="overview" src="../../images/overview2.jpg" alt="sfeer image"></li><div class="flexcontainer1"><h3 class="flex-item1">KVM</h3><h3 class="flex-item1">0-0</h3><h3 class="flex-item1">Lierse</h3></div></a>
-                <a href="#"><li class="flex-item"><img class="overview" src="../../images/overview2.jpg" alt="sfeer image"></li><div class="flexcontainer1"><h3 class="flex-item1">KVM</h3><h3 class="flex-item1">0-0</h3><h3 class="flex-item1">Lierse</h3></div></a>
-                <a href="#"><li class="flex-item"><img class="overview" src="../../images/overview2.jpg" alt="sfeer image"></li><div class="flexcontainer1"><h3 class="flex-item1">KVM</h3><h3 class="flex-item1">0-0</h3><h3 class="flex-item1">Lierse</h3></div></a>
-             </ul>
+                <hr>
+                <ul>
+                    @foreach($videos as $video)
+
+                        <a href="/herbekijk/{{$video->video_id}}" class="flex-item">
+                            <li class="flex-item">
+                                <img class="overview" src="../../images/overview2.jpg" alt="sfeer image">
+                            </li>
+                            <div class="flexcontainer1">
+                                <h3 class="flex-item1">{{$video->thuisPloeg}}</h3>
+                                <h3 class="flex-item1">{{$video->stand}}</h3>
+                                <h3 class="flex-item1">{{$video->uitPloeg}}</h3>
+                            </div>
+                        </a>
+                    @endforeach
+                </ul>
+            </ul>
         </div>
     </div>
-</div>
+    </div>
 @endsection
