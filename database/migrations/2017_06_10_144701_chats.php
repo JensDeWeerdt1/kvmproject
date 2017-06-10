c744fc86173beec1255626e8e6e2d4881e6d439f
Fabio Van Dooren <fabio.vandooren@hotmail.com>
2017-06-10 16:48:40 +0200

    migration chat toegevoegd

---
+++ database/migrations/2017_06_10_144701_chats.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateChatsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('chats', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->text('chat');
+            $table->integer('video_id')->unsigned();
+            $table->foreign('video_id')->references('video_id')->on('videos')->onDelete('cascade');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('chats');
+    }
+}
