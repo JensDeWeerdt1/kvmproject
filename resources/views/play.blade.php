4edb6523a041a2b669e9180ede4b42c68b452aa9
Fabio Van Dooren <fabio.vandooren@hotmail.com>
2017-06-10 16:35:16 +0200

    play screen blade geupdate

--- resources/views/play.blade.php
+++ resources/views/play.blade.php
@@ -1,15 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
+    <div class="container videoplayer">
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
-
-                <iframe  class="videoplayer" width="560" height="315" src="{{ $videoSrc }}" frameborder="0" allowfullscreen></iframe>
+                <iframe width="560" height="315" src="{{ $videoSrc }}" frameborder="0" allowfullscreen></iframe>
                 <h2 class="videotitles">Football 360</h2>
             </div>
         </div>
-        <form class="" action="{{ URL('/herbekijk/chat') }}" method="post">
+
+        @foreach($chats as $chat)
+            <p>{{$chat->name}}</p>
+            <p>{{$chat->chat}}</p>
+        @endforeach
+
+        <form class="" action="/herbekijk/{{$videoid}}/chat" method="post">
+            <input type="hidden" name="_token" value="{{ csrf_token() }}">
             <input type="text" name="chat" id="chat" class="form-control" placeholder="chat text" value="" required><br/>
             <input type="submit" name="submit" class="submit btn btn-primary" id="chat_submit" value="Submit">
         </form>
