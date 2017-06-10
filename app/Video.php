7659b48a4a21548ae8c232703ccf66738d1411e0
Fabio Van Dooren <fabio.vandooren@hotmail.com>
2017-06-10 16:26:36 +0200

    video model geupdate

--- app/Video.php
+++ app/Video.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Video extends Model
 {
+    public function chat(){
+        return $this->hasMany('App\Chat');
+    }
+
     protected $fillable = [
         'video_id', 'thuisPloeg', 'uitPloeg', 'stand', 'videoSrc',
     ];
