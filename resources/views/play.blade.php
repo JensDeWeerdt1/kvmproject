f2598277741742d4ab680806f59f6e7e6c0775f4
Jens De Weerdt <jdwvideos@gmail.com>
2017-06-10 17:43:40 +0200

    videoplayer fix mobile friendly

--- resources/views/play.blade.php
+++ resources/views/play.blade.php
@@ -1,10 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container videoplayer">
+    <div class="container">
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
-                <iframe width="560" height="315" src="{{ $videoSrc }}" frameborder="0" allowfullscreen></iframe>
+
+                <iframe class="videoplayer" width="560" height="315" src="{{ $videoSrc }}" frameborder="0" allowfullscreen></iframe>
+                
                 <h2 class="videotitles">Football 360</h2>
             </div>
         </div>
