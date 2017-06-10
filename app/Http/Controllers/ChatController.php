95dcf82417750436ad60fa90fcffc985f5a0c80f
Fabio Van Dooren <fabio.vandooren@hotmail.com>
2017-06-10 16:24:49 +0200

    chatcontroller geupdate

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -1,15 +1,23 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Jens
- * Date: 10/06/2017
- * Time: 15:43
- */
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+use App\Chat;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Redirect;
 
-class ChatController
+class ChatController extends Controller
 {
+    public function Chat(Request $request, $id){
+        $chat = $request->chat;
+        $userid = \Auth::user()->id;
 
+        var_dump($id);
+
+        $query = DB::table('chats')->insert(
+            ['chat' => $chat, 'video_id' => $id, 'user_id' => $userid]
+        );
+        return Redirect::to('herbekijk/' . $id);
+    }
 }
