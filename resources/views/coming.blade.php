96daeb323ed36d2c6df3ecd7631d65a67948f209
Jens De Weerdt <jdwvideos@gmail.com>
2017-06-12 10:34:59 +0200

    aanwezig scherm fix

--- resources/views/coming.blade.php
+++ resources/views/coming.blade.php
@@ -5,8 +5,8 @@
         <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-            <h4>Voordat u doorgaat ...</h4>
-            <h2>Komt u in het stadion naar de match KVM - Lierse kijken?</h2>
+            <h4 class="appsubtitle1">Voordat u doorgaat ...</h4>
+            <h2 class="apptitle1">Komt u in het stadion naar de match KVM - Lierse kijken?</h2>
                 <div class="btncontainer">
                     <a class="btn btn-primary comingbtn" href="/home">
                         Ja
